34df6b97260bbc14115db4881015846b9f8001a9
Christian Brauner <christian.brauner@mailbox.org>
2015-12-02 15:11:18 +0100

    Add license to source file
    Signed-off-by: Christian Brauner <christian.brauner@mailbox.org>

--- github_set_status.sh
+++ github_set_status.sh
@@ -1,5 +1,25 @@
 #! /bin/sh
 
+# github_set_status:
+# Mark a commit/pull-request (PR) as a comment-pull-request (CPR).
+
+# Authors:
+# Christian Brauner <christian.brauner@mailbox.org>
+
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 USAGE="Usage: `basename $0` [-h] {-n username} [-t access-token] {-r repository} {-s status} {-c commit-sha}"
 
 USERNAME=
